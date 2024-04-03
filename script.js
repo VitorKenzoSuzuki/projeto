598772ea1833bc595f767b6a4848c68cd4df2692
VitorKenzoSuzuki <VK.Suzuki@outlook.com>
2024-04-03 15:08:48 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function mode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    const img = document.querySelector('#profile img')
+
+    if(html.classList.contains('light')) {
+        img.setAttribute("src", "./imagens/avatar-light.png")
+    } else {
+        img.setAttribute("src", "./imagens/avatar.png")
+    }
+    
+
+   /* if(html.classList.contains('light')) {
+        html.classList.remove('light')
+    } else {
+        html.classList.add('light')
+    }
+}
+*/
+
+}
